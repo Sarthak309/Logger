07bd1866547fce81e735679a1d10d31548856946
Sarthak Agrawal <103929885+Sarthak309@users.noreply.github.com>
2024-09-25 14:14:50 +0530

    update

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+open class Logger{
+    public init(){}
+    
+    public static func infoPrint(file: String = #file, line: UInt = #line, function: StaticString = #function, _ items: Any = "") {
+        Swift.print("<---Printing information about \(items) at line: \(line) in file: \((file as NSString).lastPathComponent) for the function: \(function)", terminator: ": End---->")
+    }
+    
+    public static func debugPrint(file: String = #file, line: UInt = #line, function: StaticString = #function, _ items: Any = ""){
+        Swift.print("<---Printing Debug information about \(items) at line: \(line) in file: \((file as NSString).lastPathComponent) for the function: \(function)", terminator: ": End---->")
+    }
+}
--- Tests/LoggerTests/LoggerTests.swift
+++ Tests/LoggerTests/LoggerTests.swift
@@ -1,6 +1,18 @@
 import Testing
+import XCTest
 @testable import Logger
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class LoggerTests: XCTestCase {
+    func testInfoPrint() {
+        Logger.infoPrint("Hello, World!")
+    }
+    
+    func testDebugPrint() {
+        Logger.debugPrint("Hello, World!")
+    }
+    
+    func testEmptyString(){
+        Logger.debugPrint()
+        Logger.infoPrint()
+    }
 }
