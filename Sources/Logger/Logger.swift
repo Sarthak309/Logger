aa429a6d4b0aa256dddf361cf515de314804e663
Sarthak Agrawal <103929885+Sarthak309@users.noreply.github.com>
2024-09-25 14:42:27 +0530

    update

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -6,11 +6,11 @@ import Foundation
 open class Logger{
     public init(){}
     
-    public static func infoPrint(file: String = #file, line: UInt = #line, function: StaticString = #function, _ items: Any = "") {
-        Swift.print("<---Printing information about \(items) at line: \(line) in file: \((file as NSString).lastPathComponent) for the function: \(function)", terminator: ": End---->")
+    public static func infoPrint(file: String = #file, line: UInt = #line, function: StaticString = #function, _ items: Any = "code") {
+        Swift.print("<---Information: \(items) at line: \(line) in file: \((file as NSString).lastPathComponent) for the function: \(function)", terminator: ": End---->")
     }
     
-    public static func debugPrint(file: String = #file, line: UInt = #line, function: StaticString = #function, _ items: Any = ""){
-        Swift.print("<---Printing Debug information about \(items) at line: \(line) in file: \((file as NSString).lastPathComponent) for the function: \(function)", terminator: ": End---->")
+    public static func debugPrint(file: String = #file, line: UInt = #line, function: StaticString = #function, _ items: Any = "code"){
+        Swift.print("<---Debug: \(items) at line: \(line) in file: \((file as NSString).lastPathComponent) for the function: \(function)", terminator: ": End---->")
     }
 }
